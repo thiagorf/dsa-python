3929dcbc2d5666199599aca6105b640255cb4e6b
thiagorf <trfarciroli@gmail.com>
2023-01-30 11:49:32 -0300

    add bubble-sort algorithm

---
+++ algorithms/bubble-sort.py
@@ -0,0 +1,26 @@
+
+
+def bubbleSort(arr):
+	steps = 0
+
+	for i in range(len(arr)):
+		# 0,1,2,3,4 = len(5)
+		for j in range(0, len(arr) - i - 1):
+			# i = 0
+			# j in range(0, len(arr) - 0 - 1)
+			# j in range(0, 5 - 0 - 1)
+			# j in range(0, 4) = 0,1,2
+			if arr[j] > arr[j + 1]:
+				steps += 1
+				temp = arr[j]
+				arr[j] = arr[j + 1]
+				arr[j+1] = temp
+				print("Step: %i" % steps)
+
+
+
+data = [-2,45,0,11,-9]
+
+bubbleSort(data)
+
+print(data)
--- doubly-linked-list.py
+++ doubly-linked-list.py
@@ -27,16 +27,22 @@
 class DoublyLinkedList:
 	def __init__(self, head = None):
 		self.head = head
+		self.size = 0 if head == None else 1 
 
 	def add_beginning(self, node):
-		
-		node.next = self.head
-		self.head.prev = node
-		self.head = node
+		if self.size == 0:
+			self.head = node
+			self.size += 1
+		else:
+			node.next = self.head
+			self.head.prev = node
+			self.head = node
+			self.size += 1
 		
 	def add_after(self, targetNode, newNode):
 		# None <- b <-> a -> None
 		node = self.head
+		initialSize = self.size
 		while node is not None:
 			if node.data == targetNode.data:
 				nextNode = node.next
@@ -44,8 +50,13 @@ class DoublyLinkedList:
 				node.next = newNode
 				nextNode.prev = newNode
 				node = None
+				self.size += 1
 			else:
 				node = node.next
+		if initialSize == self.size:
+			# If the size doesn't change, it means that the target node doesn't exist
+			print("Invalid target node")
+	
 
 	def remove_beginning(self):
 		pass
@@ -86,4 +97,11 @@ linkedList = DoublyLinkedList(Node("a"))
 nodeB = Node("b")
 linkedList.add_beginning(nodeB)
 linkedList.add_after(nodeB, Node("c"))
+linkedList.add_after(Node("sdsa"), Node("sdasdf"))
 print(linkedList.show())
+print("list size: %i" % linkedList.size)
+
+emptyList = DoublyLinkedList()
+emptyList.add_beginning(Node("h"))
+print(emptyList.show())
+print(emptyList.size)
