a3af9b795d3d4fbbc0d559c0b1eee8eb6a110190
thiagorf <trfarciroli@gmail.com>
2023-02-06 15:55:08 -0300

    avl tree remove method(height bug)

--- avl-tree.py
+++ avl-tree.py
@@ -44,43 +44,86 @@ class AvlTree:
         # {-1, 0, 1}
         balance = self.findBalance(root)
 
-		# balance factor > 1
-		# tree is left heavy
+        # balance factor > 1
+        # tree is left heavy
         if balance > 1:
             if value < root.left.data:
                 return self.rightRotate(root.right)
             else:
-				# left-right rotate is only possible
-				# when the left child of a node is less than the new node
-				"""
-				       21  <- root in this subtree
-					  /
-				     11	<- left child of root
-					  \
-				   	  12 < - new Node
-				"""
+                # left-right rotate is only possible
+                # when the left child of a node is less than the new node
+                """
+                       21  <- root in this subtree
+                      /
+                     11	<- left child of root
+                      \
+                         12 < - new Node
+                """
                 root.left = self.leftRotate(root.left)
 
                 return self.rightRotate(root.right)
 
-		# balance factor < -1
-		# tree is right heavy
+        # balance factor < -1
+        # tree is right heavy
         if balance < -1:
             if value > root.right.data:
                 return self.leftRotate(root.left)
             else:
-				# right-left rotate is only possible
-				# when the right child of a node is greater than the new node
-				"""
-				       21  <- root in this subtree
-					    \
-				        36	<- right child of root
-					    /
-				   	  28 < - new Node
-				"""
+                # right-left rotate is only possible
+                # when the right child of a node is greater than the new node
+                """
+                       21  <- root in this subtree
+                        \
+                        36	<- right child of root
+                        /
+                         28 < - new Node
+                """
                 root.right = self.rightRotate(root.right)
 
                 return self.leftRotate(root.left)
+        return root
+
+    def removeNode(self, root, value):
+        if not root:
+            return None
+
+        if value > root.data:
+            root.right = self.removeNode(root.right, value)
+        elif value < root.data:
+            root.left = self.removeNode(root.left, value)
+        else:
+            if not root.right and not root.left:
+                return None
+            if not root.left and root.right:
+                return root.right
+            if not root.right and root.left:
+                return root.left
+            if root.left and root.right:
+                temp = root.right
+
+                while temp.left:
+                    temp = temp.left
+
+                root.data = temp.data
+                root.right = self.removeNode(root.right, root.data)
+
+        root.height = max(self.findHeight(root.left), self.findHeight(root.right))
+
+        balance = self.findBalance(root)
+
+        if balance > 1:
+            if self.findBalance(root.left) >= 0:
+                return self.rightRotate(root)
+            else:
+                root.left = self.leftRotate(root.left)
+                return self.rightRotate(root)
+        if balance < -1:
+            if self.findBalance(root.right) <= 0:
+                return self.leftRotate(root)
+            else:
+                root.right = self.rightRotate(root.right)
+
+                return self.leftRotate(root)
         return root
 
     def leftRotate(self, x):
@@ -109,7 +152,7 @@ class AvlTree:
 
     def findHeight(self, node):
         if node == None:
-            return -1
+            return 0
         return node.height
 
     def findBalance(self, node):
@@ -140,3 +183,8 @@ print("Next")
 avlTree.add_node(root, 12)
 print("Node 9 balance: %i" % avlTree.findBalance(root.left))
 avlTree.inorder(root)
+print("\nRemove node 9")
+avlTree.removeNode(root, 9)
+avlTree.inorder(root)
+print("\n")
+print(avlTree.findHeight(root.left.left.right))
